92a7ec8e6b655e2bbe8796027c19d593db292294
Fernando Aurélio da Silveira <fernando.silveira@softexpert.com>
2018-12-19 13:28:12 -0200

    Animação e mudança de cor para produtos selecionados

--- wp-content/themes/shop-isle/library/utils.js
+++ wp-content/themes/shop-isle/library/utils.js
@@ -2,4 +2,12 @@ jQuery(document).ready(function(){
 	jQuery('.item-menu').on('hover', function(){		
 		jQuery('.item-menu .menu-account').toggleClass('esconder');
 	});
+
+	jQuery(".carrinho").on("click", function(){
+		if (jQuery(this).hasClass("added")) {
+			jQuery(this).css("background-color","black");
+		} else {
+			jQuery(this).css("background-color","green");
+		}
+	});	
 });
